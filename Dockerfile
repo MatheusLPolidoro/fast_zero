992e9ef2d7a8b8ed92c02b58e1239bff385f3436
MatheusLPolidoro <mattpolidoro4@gmail.com>
2024-08-15 19:25:05 -0300

    Ajuste devido formato de DATABASE_URL.

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,5 @@ RUN pip install poetry
 RUN poetry config installer.max-workers 10
 RUN poetry install --no-interaction --no-ansi
 
-
 EXPOSE 8000
 CMD poetry run uvicorn --host 0.0.0.0 fast_zero.app:app
--- fast_zero/database.py
+++ fast_zero/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import Session
 
 from fast_zero.settings import Settings
 
-engine = create_engine(Settings().DATABASE_URL)
+# Certifique-se de que a URL do banco de dados seja uma string
+database_url = Settings().DATABASE_URL
+
+if isinstance(database_url, bytes):
+    database_url = database_url.decode('utf-8')
+
+engine = create_engine(database_url)
 
 
 def get_session():  # pragma: no cover
